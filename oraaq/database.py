2b354a83eedd1bfd0d2bddae736cb6f6113c6b2e
syedsajjadali258 <syedsajjadali258@gmail.com>
2025-02-22 01:12:06 +0500

    2nd Commit Sajjad

--- oraaq/database.py
+++ oraaq/database.py
@@ -4,8 +4,8 @@ from core.config import settings
 # Database connection
 def get_db_connection():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="sajjad",
-        database="oraaqdb"
+        host=settings.DB_HOST,
+        user=settings.DB_USER,
+        password=settings.DB_PASSWORD,
+        database=settings.DB_NAME
     )
